0876d97ae43ac79463873c896ae802dca868c3e5
Oddy Virgantara Putra <oddy@unida.gontor.ac.id>
2019-03-06 10:53:28 +0700

    Init project

---
+++ src/textpreprocessing/TextPreprocessing.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textpreprocessing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author oddy
+ */
+public class TextPreprocessing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String kalimat1 = "Budi makan nasi goreng.";
+        String kalimat2 = "Susi makan telur goreng.";
+
+        String kalimat = kalimat1 + " " + kalimat2;
+
+        System.out.println(kalimat);
+
+        StringTokenizer st = new StringTokenizer(kalimat);
+
+        List<String> listKata = new ArrayList<String>();
+        int[] frekuensi = new int[st.countTokens()];
+        List<String> listKataSemua = new ArrayList<String>();
+
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken().toLowerCase();
+            listKataSemua.add(s);
+        }
+
+        int isVisited = -1;
+
+        for (int i = 0; i < listKataSemua.size(); i++) {
+
+            String s = listKataSemua.get(i);
+            int counter = 1;
+
+            for (int j = i + 1; j < listKataSemua.size(); j++) {
+                String s2 = listKataSemua.get(j);
+
+                if (s.equals(s2)) {
+                    counter++;
+                    frekuensi[j] = isVisited;
+                }
+            }
+
+            if (frekuensi[i] != isVisited) {
+                frekuensi[i] = counter;
+            }
+
+        }
+
+        for (int i = 0; i < frekuensi.length; i++) {
+            if (frekuensi[i] != isVisited) {
+                System.out.println(listKataSemua.get(i) + " : " + frekuensi[i]);
+            }
+        }
+    }
+
+}
